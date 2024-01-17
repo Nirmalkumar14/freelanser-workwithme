faaa7f03a4af44249df5a657d4a2a93745f5b181
Nirmalkumar14 <110583160+Nirmalkumar14@users.noreply.github.com>
2024-01-17 21:57:34 +0530

    changes

---
+++ index.js
@@ -0,0 +1,13 @@
+var sidenav = document.querySelector(".side-navbar")
+// sidenav.style.display ="none";
+
+
+function shownavbar(){
+    sidenav.style.left ="0";
+    
+}
+
+function closenavbar(){
+    sidenav.style.left ="-60%";
+         
+}
